d52b2ec76f077bb14508b86a58a852e1803c1c0c
Mahmudul Hasan <mahmudnotes@outlook.com>
2022-08-14 12:59:38 +0600

    Dynamic return type.

--- DirectX/Awesome/WinMain.cpp
+++ DirectX/Awesome/WinMain.cpp
@@ -9,7 +9,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParm, LPARAM lParm)
 	case WM_CLOSE:
 		PostQuitMessage(69);
 		break;
-	default: ;
+	default:
+		break;
 	}
 	return DefWindowProc(hWnd, msg, wParm, lParm);
 }
@@ -20,32 +21,34 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	const auto pClassName = L"awesome3d";
 
 	// Register window class
-	WNDCLASSEXW wca = { 0 };
-	wca.cbSize = sizeof(wca);
-	wca.style = CS_OWNDC;
-	wca.lpfnWndProc = WndProc;
-	wca.cbClsExtra = 0;
-	wca.cbWndExtra = 0;
-	wca.hInstance = hInstance;
-	wca.hIcon = nullptr;
-	wca.hCursor = nullptr;
-	wca.hbrBackground = nullptr;
-	wca.lpszMenuName = nullptr;
-	wca.hIconSm = nullptr;
-	wca.lpszClassName = pClassName;
-	RegisterClassExW(&wca);
+	WNDCLASSEXW wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.hIconSm = nullptr;
+	wc.lpszClassName = pClassName;
+	RegisterClassExW(&wc);
 
 	// Create window instance
-	const HWND hwnd = CreateWindowExW(0, pClassName, L"Awesome Window",
+	const HWND hWnd = CreateWindowExW(0, pClassName, L"Awesome Window",
 		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, 200, 200, 640, 480,
 		nullptr, nullptr, hInstance, nullptr);
-	ShowWindow(hwnd, nCmdShow);
+	ShowWindow(hWnd, nCmdShow);
 	MSG msg;
-
-	while (GetMessageW(&msg, hwnd, 0, 0) > 0)
+	BOOL gResult;
+	while ((gResult = GetMessageW(&msg, hWnd, 0, 0)) > 0)
 	{
 		TranslateMessage(&msg);
 		DispatchMessageW(&msg);
 	}
-	return 0;
+	if (gResult == -1)
+		return -1;
+	return msg.wParam;
 }
